1871459242a0b450a2a3564c84ac45a5a4215c71
liviarez <mrezende.livia@gmail.com>
2023-05-20 19:50:36 -0300

    Feat: Req 1. Implemets Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Login from './Pages/Login';
+import Search from './Pages/Search';
+import Album from './Pages/Album';
+import Favorites from './Pages/Favorites';
+import Profile from './Pages/Profile';
+import ProfileEdit from './Pages/ProfileEdit';
+import NotFound from './Pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="*" component={ NotFound } />
+        </Switch>
+
+      </div>
+    );
   }
 }
 
