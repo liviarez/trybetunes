790216f037f78a04a0978e0ec6dbfe66ef03147a
liviarez <mrezende.livia@gmail.com>
2023-05-29 10:57:21 -0300

    Feat: Req 5. Implemented  search artist form

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    isButtonSearchDisable: true,
+  };
+
+  validateSearchButton = ({ target }) => {
+    console.log('search target', target);
+    const minimumCharacters = 2;
+    const inputValue = target.value;
+    this.setState({
+      isButtonSearchDisable: inputValue.length < minimumCharacters,
+    });
+  };
+
   render() {
+    const { isButtonSearchDisable } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            placeholder="Nome do Artista"
+            onChange={ this.validateSearchButton }
+          />
+          <button
+            data-testid="search-artist-button"
+            disabled={ isButtonSearchDisable }
+          >
+            Pesquisar:
+          </button>
+        </form>
       </div>
     );
   }
