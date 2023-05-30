07cdff15d85bc7a8888b10a0e94e3e063edec02b
liviarez <mrezende.livia@gmail.com>
2023-05-30 02:19:02 -0300

    Fixed: Req 6 7

--- src/Pages/ProfileEdit.js
+++ src/Pages/ProfileEdit.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+/* import { Redirect } from 'react-router-dom/cjs/react-router-dom'; */
 import Header from '../components/Header';
+/* import Loading from '../components/Loading';
+import { getUser, updateUser } from '../services/userAPI'; */
 
 export default class ProfileEdit extends Component {
   render() {
--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,66 +1,87 @@
 import React, { Component } from 'react';
-import Header from '../components/Header';
-import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Loading from './Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Pesquisar from '../components/Pesquisar';
 
-export default class Search extends Component {
+class Searchs extends Component {
   state = {
     isButtonSearchDisable: true,
-    artistName: '',
-    savedArtistName: '',
+    name: '',
+    albums: '',
     isLoading: false,
-    resultAPI: [],
+  };
+
+  saveButton = () => {
+    this.setState({
+      isLoading: true,
+    }, async () => {
+      const { name } = this.state;
+      const albunsArtistas = await searchAlbumsAPI(name);
+      this.setState({
+        isLoading: false,
+        albums: albunsArtistas,
+      });
+    });
   };
 
   validateSearchButton = ({ target }) => {
     const minimumCharacters = 2;
-    const inputValue = target.value;
-    this.setState({
-      isButtonSearchDisable: inputValue.length < minimumCharacters,
-    });
-  };
-
-  requestSearchAlbum = async () => {
-    const { artistName } = this.state;
-    this.setState({
-      isLoading: true,
-      savedArtistName: artistName,
-    });
-    const teste = await searchAlbumsAPI(artistName);
-    this.setState({
-      artistName: '',
-      isLoading: false,
-      resultAPI: teste,
-    });
+    const { name, value } = target;
+    if (value.length >= minimumCharacters) {
+      this.setState(() => ({
+        [name]: value,
+        isButtonSearchDisable: false,
+      }));
+    }
   };
 
   render() {
-    const { isButtonSearchDisable, resultAPI, isLoading, savedArtistName } = this.state;
+    const { isButtonSearchDisable, isLoading, albums, name } = this.state;
     return (
       isLoading ? <Loading /> : (
-        <div data-testid="page-search">
-          <Header />
+        <div>
           <form>
-            <input
-              data-testid="search-artist-input"
-              placeholder="Nome do Artista"
-              onChange={ this.validateSearchButton }
-              value={ artist }
-              name="artist"
-            />
-            <button
-              data-testid="search-artist-button"
-              disabled={ isButtonSearchDisable }
-              onClick={ this.requestSearchAlbum }
-              type="button"
-            >
-              Pesquisar:
-            </button>
+            <label>
+              <input
+                data-testid="search-artist-input"
+                type="text"
+                name="name"
+                placeholder="Nome do artista"
+                onChange={ this.validateSearchButton }
+              />
+              <button
+                data-testid="search-artist-button"
+                disabled={ isButtonSearchDisable }
+                onClick={ this.saveButton }
+              >
+                Pesquisar
+              </button>
+              <p>
+                Resultado de álbuns de:
+                {' '}
+                {name}
+              </p>
+            </label>
+            {
+              albums !== '' && (
+                <div>
+                  {albums.length !== 0
+                    ? (
+                      <div>
+                        {albums.map((cart) => (<Pesquisar
+                          key={ cart.collectionId }
+                          id={ cart.collectionId }
+                          imagem={ cart.artworkUrl100 }
+                          name={ cart.collectionName }
+                        />))}
+                      </div>
+                    ) : (<p>Nenhum álbum foi encontrado</p>)}
+                </div>)
+            }
           </form>
         </div>
-
       )
     );
   }
 }
+export default Searchs;
